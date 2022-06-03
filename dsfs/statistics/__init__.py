688f6b05850d8b34c21ebe9cae4858f99a0aafb7
Demetrius Almada <demetrius.almada@gmail.com>
2022-06-03 14:18:16 -0700

    Add statistics module
    The statistics module defines several functions used to compute several
    metrics that describe a dataset.
    
    mean: Computes the average value of a dataset.
    
    median: Computes the middle value using two private methods
    (_median_odd and _median_even).
    
    quantile: Computes the pth-percentie value of a dataset.
    
    mode: Returns a list of the most common value(s) in a dataset.
    
    data_range: Computes the range of a dataset.
    
    standardize: Rescales the dataset such that the mean is zero.
    
    variance: Computes the squared deviation from the mean.
    
    standard_deviation: Computes the average deviation from the mean.
    
    interquartile_range: Computes the difference between the 75%-ile and the
    25%-ile.
    
    covariance: Computes the variance between two variables and their mean in
    tandem.
    
    correlation: Computes the amount that two variables are linearly related.

---
+++ dsfs/statistics/__init__.py
@@ -0,0 +1,13 @@
+from .statistics import (
+    mean,
+    median,
+    quantile,
+    mode,
+    data_range,
+    standardize,
+    variance,
+    standard_deviation,
+    interquartile_range,
+    covariance,
+    correlation,
+)
---
+++ dsfs/statistics/statistics.py
@@ -0,0 +1,94 @@
+from collections import Counter
+from dsfs.linalg import (
+    Vector,
+    dot,
+)
+from typing import (
+    List,
+)
+import math
+
+
+def mean(v: Vector) -> float:
+    """Computes the average of a list of values"""
+    return sum(v) / len(v)
+
+
+def _median_odd(v: Vector) -> float:
+    """Computes the median when the quantity of data is odd"""
+    return sorted(v)[len(v) // 2]
+
+
+def _median_even(v: Vector) -> float:
+    """Computes the median when the quantity of data is odd"""
+    sorted_v = sorted(v.data)
+    hi_midpoint = len(v) // 2
+    return (sorted_v[hi_midpoint - 1] + sorted_v[hi_midpoint]) / 2
+
+
+def median(v: Vector) -> float:
+    return _median_even(v) if len(v) % 2 == 0 else _median_odd(v)
+
+
+def quantile(v: Vector, p: float) -> float:
+    """Returns the pth-percentile value in the input data"""
+    p_index = int(p * len(v))
+    return sorted(v)[p_index]
+
+
+def mode(v: Vector) -> List[float]:
+    """Returns a list of the most common value(s) in the input data"""
+    counts = Counter(v)
+    max_count = max(counts.values())
+    return [v_i for v_i, count in counts.items() if count == max_count]
+
+
+def data_range(v: Vector) -> float:
+    """Returns the range of the input data"""
+    return max(v) - min(v)
+
+
+def standardize(v: Vector) -> Vector:
+    """Computes the standard score such that the mean becomes zero"""
+    v_bar = mean(v)
+    return Vector([v_i - v_bar for v_i in v])
+
+
+def variance(v: Vector) -> float:
+    """"""
+    assert len(v) >= 2, "Variance requires at least two data points"
+
+    num_elements = len(v)
+    standard_score = standardize(v)
+    return standard_score.sum_of_squares() / (num_elements - 1)
+
+
+def standard_deviation(v: Vector) -> float:
+    """Computes the standard deviation as the square root of the variance"""
+    return math.sqrt(variance(v))
+
+
+def interquartile_range(v: Vector) -> float:
+    """Returns the difference between the 75%-ile and the 25%-ile"""
+    return quantile(v, 0.75) - quantile(v, 0.25)
+
+
+def covariance(v: Vector, w: Vector) -> float:
+    assert len(v) == len(w), "The inputs must have the same length"
+
+    return dot(standardize(v), standardize(w)) / (len(v) - 1)
+
+
+def correlation(v: Vector, w: List[float]) -> float:
+    """Measures how much the input lists vary in tandem about their means"""
+    stdev_v = standard_deviation(v)
+    stdev_w = standard_deviation(w)
+    if stdev_v > 0 and stdev_w > 0:
+        return covariance(v, w) / stdev_v / stdev_w
+    else:
+        return 0
+
+
+def chi_squared(v: Vector) -> float:
+    """"""
+    pass
