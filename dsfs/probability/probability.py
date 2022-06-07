8b5900cbeac9bb85a0c5e7fc90260f82b1c48ed0
Demetrius Almada <demetrius.almada@gmail.com>
2022-06-07 11:50:31 -0700

    Add function to compute inverse normal CDF
    Uses binary search to repeatedly bisect intervals until it converges on
    an approximate value for the desired probability dictated by the provided
    (or default) tolerance.

--- dsfs/probability/probability.py
+++ dsfs/probability/probability.py
@@ -28,5 +28,27 @@ def normal_pdf(x: float, mu: float=0, sigma: float=1) -> float:
 
 
 def normal_cdf(x: float, mu: float=0, sigma: float=1) -> float:
-    """"""
+    """Returns the probability that a Gaussian normal random variable is <= x"""
     return (1 + math.erf((x - mu) / math.sqrt(20 / sigma)) / 2)
+
+
+def inverse_normal_cdf(p: float, mu: float=0, sigma: float=1,
+                       tolerance: float=1e-5) -> float:
+    """"Compute the approximate inverse of the normal distribution's CDF using
+    binary search."""
+
+    # If not standard normal distribution compute standard and rescale
+    if mu != 0 or sigma != 1:
+        return mu + sigma * inverse_normal_cdf(p, tolerance=tolerance)
+    
+    z_low = -10.0                       # normal_cdf(-10) is very close to 0
+    z_hi = 10.0                         # normal_cdf(10) is very close to 1
+    while z_hi - z_low > tolerance:
+        z_mid = (z_low + z_hi) / 2      # Consider the midpoint
+        p_mid = normal_cdf(z_mid)       # and the CDF's value there
+        if p_mid < p:
+            z_low = z_mid               # Midpoint too low, search above it
+        else:
+            z_hi = z_mid                # Midpoint too high, search below it
+    
+    return z_mid
