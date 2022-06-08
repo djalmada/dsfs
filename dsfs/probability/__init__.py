0ad5bb76ef9cfb859673f93571b465661aab9354
Demetrius Almada <demetrius.almada@gmail.com>
2022-06-08 16:47:30 -0700

    Update __init__.py
    The __init__.py for the probability module now includes the remaining
    functions added from probability.py and inference.py.

--- dsfs/probability/__init__.py
+++ dsfs/probability/__init__.py
@@ -2,4 +2,16 @@ from .probability import (
     uniform_pdf,
     uniform_cdf,
     normal_pdf,
+    normal_cdf,
+    inverse_normal_cdf,
+    bernoulli_trial,
+    binomial,
+)
+from .inference import (
+    normal_approximation_to_binomial,
+    normal_probability_above,
+    normal_probability_between,
+    normal_upper_bound,
+    normal_lower_bound,
+    normal_two_sided_bounds,
 )
